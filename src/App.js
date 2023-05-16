56956ae57745e122394de8fc0042a6243151ad06
Irina Voropayeva <i.vvrpv@gmail.com>
2023-05-16 16:05:36 +0300

    added products and shopping icon & contexts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
-import Shop from '././components/shop/shop.component';
+import Shop from './routes/shop/shop.component';
 import Authentication from './routes/authentication/authentication.component';
 
 const App = () => {
--- src/components/shop/shop.component.jsx
+++
@@ -1,9 +0,0 @@
-const Shop = () => {
-    return(
-        <div>
-            <h1>Shop component</h1>
-        </div>
-    )
-}
-
-export default Shop;
